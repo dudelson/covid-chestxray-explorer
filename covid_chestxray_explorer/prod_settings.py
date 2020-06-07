cac5c56f0e87d5a4ecacb07954af9308c27782e3
David Udelson <dru5@cornell.edu>
2020-06-07 19:23:55 -0400

    Debug production settings

--- covid_chestxray_explorer/prod_settings.py
+++ covid_chestxray_explorer/prod_settings.py
@@ -1,24 +1,25 @@
+import os
 from .default_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# TODO
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['DJANGO_PROD_HOST']]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # TODO: change to postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
+
+# static files
+# TODO are these settings actually necessary for my app? (I suspect not.)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
